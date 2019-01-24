a93408329d3d604dcdfc5541c5075469386c316f
k.takenaka <takenaka@grofield.jp>
2019-01-24 19:28:41 +0900

    [update] 疲れた版

--- config.fish
+++ config.fish
@@ -1,7 +1,30 @@
 alias vi='/usr/bin/vim'
 alias open='explorer.exe'
 
-alias rm='rmtrash'
+alias vf='vim ~/.config/fish/config.fish'
+alias sf='source ~/.config/fish/config.fish'
 
-# theme設定
-set -g theme_date_format '+%T %Y/%m/%d'
+# ゴミ箱
+if [ -d $HOME/.Trash ]
+  alias rm='mv --backup=numbered --target-directory=$HOME/.Trash'
+end
+
+# プロンプト設定
+# function fish_prompt
+#  ~/powerline-shell.py $status --shell bare ^/dev/null
+# end
+
+# set fish_function_path $fish_function_path "/usr/local/lib/python2.7/dist-packages/powerline/bindings/fish"
+# powerline-setup
+
+function fish_greeting
+  echo '><))"> Gyo gyo.'
+end
+
+function fish_prompt
+  printf '\n%s%s%s\n><))"> ' (set_color #fish_color_cwd) (prompt_pwd) (set_color normal)
+end
+
+function fish_right_prompt
+  date '+[%T %Y/%m/%d]'
+end
