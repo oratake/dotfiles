99cb4d36a040272cab66efb5cf6cbd3803957452
oratake <smith.shimomura@gmail.com>
2019-09-25 16:45:31 +0900

    add: ln sf script

---
+++ dot_update.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
+ln -sf ~/dotfiles/.gitignore_global ~/.gitignore_global
+ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
+ln -sf ~/dotfiles/fish/ ~/.config/fish/
+ln -sf ~/dotfiles/nvim/ ~/.config/nvim/
