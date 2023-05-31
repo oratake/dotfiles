8e5d4eb91391cd616b64ffcf0ac2423e3af1ac42
Kyosuke Takenaka <smith.shimomura@gmail.com>
2023-05-31 13:44:14 +0900

    vim: astronvim: keymapping for buffer and setting change

--- vim/astronvim/user/mappings.lua
+++ vim/astronvim/user/mappings.lua
@@ -1,7 +1,11 @@
 return {
   n = {
-    ["<Leader>r"] = { "<cmd>AstroReload<cr>", desc = "Reload AstroNvim conf" },
+    -- user settings
+    ["<Leader>,"] = { desc = "⚙Settings" },
+    ["<Leader>,r"] = { "<cmd>AstroReload<cr>", desc = "Reload AstroNvim conf" },
+    ["<Leader>,e"] = { "<cmd>e ~/.config/nvim/lua/user/<cr>", desc = "User config dir." },
 
+    -- screen control
     ["s"] = { "<Nop>", name = "Screen control" },
     ["sj"] = { "<C-w>j", desc = "Move down" },
     ["sk"] = { "<C-w>k", desc = "Move up" },
@@ -9,9 +13,12 @@ return {
     ["sl"] = { "<C-w>l", desc = "Move right" },
     ["sr"] = { "<C-w>r", desc = "Rotate" },
     ["sq"] = { "<cmd>q<cr>", desc = "Quit" },
+    ["sQ"] = { "<cmd>bd<cr>", desc = "Quit buffer" },
     ["sv"] = { "<cmd>vsp<cr>", desc = "Split Vertical |" },
     ["ss"] = { "<cmd>sp<cr>", desc = "Split Holizontal -" },
     ["sn"] = { "gt", desc = "Next tab" },
     ["sp"] = { "gT", desc = "Prev tab" },
+    ["sN"] = { "<cmd>bn<cr>", desc = "Next Buffer" },
+    ["sP"] = { "<cmd>bp<cr>", desc = "Prev Buffer" },
   },
 }
