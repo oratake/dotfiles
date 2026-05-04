b7a9c96997e587b8e010f745963c885192aa6b82
oratake <smith.shimomura@gmail.com>
2026-05-04 19:52:32 +0900

    vim astrovim init

--- vim/astronvim/user/usercfg.lua
+++ vim/astronvim/user/usercfg.lua
@@ -15,3 +15,14 @@ local servers = {
 
 vim.opt.clipboard = "unnamedplus"
 vim.opt.wrap = true
+
+local lspconfig = require("lspconfig")
+lspconfig.rust_analyzer.setup({
+  settings = {
+    ["rust-analyzer"] = {
+      check = {
+        command = "clippy",
+      },
+    },
+  },
+})
